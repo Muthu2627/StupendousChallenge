9772babf4f5fbeb2b76829c04aa258658f2b7dfe
vishal.gr <vishal.gr@zohocorp.com>
2022-06-03 15:18:26 +0530

    Added my project

---
+++ StupendousChallenge/BlankPage2.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace StupendousChallenge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage2 : Page
+    {
+        private string Roast = null;
+        private string Sweetner = null;
+        private string Cream = null;
+        public BlankPage2()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RoastItem_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (MenuFlyoutItem)sender;
+            Roast = item.Text;
+            Display();
+        }
+
+        private void SweetnerItem_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (MenuFlyoutItem)sender;
+            Sweetner = item.Text;
+            if(Sweetner == "None"){ Sweetner = null; }
+            Display();
+        }
+
+        private void CreamItem_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (MenuFlyoutItem)sender;
+            Cream = item.Text;
+            if(Cream == "None") { Cream = null; }
+            Display();
+        }
+
+        private void Display()
+        {
+            if(Roast == null || Roast == "None")
+            {
+                CoffeePageChosenText.Text = "None";
+                return;
+            }
+            CoffeePageChosenText.Text = Roast;
+            if(Sweetner != null && Sweetner != "None")
+            {
+                CoffeePageChosenText.Text += " + " + Sweetner;
+            }
+            if(Cream != null && Cream != "None")
+            {
+                CoffeePageChosenText.Text += " + " + Cream;
+            }
+        }
+    }
+}
---
+++ StupendousChallenge/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace StupendousChallenge
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            MainFrame.Navigate(typeof(BlankPage1));
+        }
+
+        private void DonutPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(BlankPage1));
+        }
+
+        private void CoffeePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(BlankPage2));
+        }
+
+        private void SchedulePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(BlankPage3));
+        }
+
+        private void CompletePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(BlankPage4));
+        }
+
+        private void MainPageNavigationButton_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            var ButtonSelected = (Button)sender;
+            ButtonSelected.Background = new SolidColorBrush(Microsoft.UI.Colors.DarkGray);
+        }
+
+        private void MainPageNavigationButton_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            var ButtonSelected = (Button)sender;
+            ButtonSelected.Background = new SolidColorBrush(Microsoft.UI.Colors.Black);
+        }
+    }
+}
